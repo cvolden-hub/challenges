eff056434fc884752b9c0325ba1106cb74a6f3ef
cvolden-hub <cvolden@doterra.com>
2019-10-21 22:27:19 -0600

    challenge5 - more trys

--- challenge2/challenge2.py
+++ challenge2/challenge2.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
+
 class Challenge2(unittest.TestCase):
 
     def setUp(self):
@@ -16,15 +17,16 @@ class Challenge2(unittest.TestCase):
     def test_challenge2(self):
         self.driver.get("https://www.copart.com")
         self.assertIn("Copart", self.driver.title)
-        searchInput = self.driver.find_element_by_id("input-search")
-        searchInput.send_keys("Exotics")
-        searchInput.send_keys(Keys.RETURN)
+        searchinput = self.driver.find_element_by_id("input-search")
+        searchinput.send_keys("Exotics")
+        searchinput.send_keys(Keys.RETURN)
         sleep(5)
 
         self.assertIn("Exotics", self.driver.title)
         elements = self.driver.find_elements(By.XPATH, "//span[contains(text(),'PORSCHE')]")
 
         assert "PORSCHE" in elements[0].text, "Whoops, I can't find the text PORSCHE"
+
 
 if __name__ == '__main__':
     unittest.main()
--- challenge3/challenge3.py
+++ challenge3/challenge3.py
@@ -2,7 +2,8 @@ import unittest
 
 from selenium import webdriver
 
-class Challenge3ForWhile(unittest.TestCase):
+
+class Challenge3(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome("../chromedriver.exe")
@@ -10,21 +11,22 @@ class Challenge3ForWhile(unittest.TestCase):
     def tearDown(self):
         self.driver.close()
 
+    # gets make
     def test_challenge3ForLoop(self):
         self.driver.get("https://www.copart.com")
         self.assertIn("Copart", self.driver.title)
 
-        makeModel = self.driver.find_elements_by_xpath("//a[starts-with(@href,'./popular/m')]")
-        #print(len(makeModel))
-        for x in makeModel:
+        makemodel = self.driver.find_elements_by_xpath("//a[starts-with(@href,'./popular/m')]")
+        for x in makemodel:
             print(x.text + " - " + x.get_attribute("href"))
 
+    # gets model
     def test_challenge3WhileLoop(self):
         self.driver.get("https://www.copart.com")
         self.assertIn("Copart", self.driver.title)
 
         categories = self.driver.find_elements_by_xpath("//a[starts-with(@href,'./popular/category')]")
-        #print(len(categories))
+
         i = 0
         while i <len(categories):
             print(categories[i].text + " - " + categories[1].get_attribute("href"))
