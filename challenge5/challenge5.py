5df97a70f4d9f4a58bdf89fca485c76ec8f692aa
cvolden-hub <cvolden@doterra.com>
2019-10-23 00:06:03 -0600

    challenge5 - more trys

--- challenge5/challenge5.py
+++ challenge5/challenge5.py
@@ -11,6 +11,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.select import Select
 from time import sleep
 
 class Challenge5(unittest.TestCase):
@@ -25,17 +26,42 @@ class Challenge5(unittest.TestCase):
         self.driver.get("https://www.copart.com")
         self.assertIn("Copart", self.driver.title)
 
-        #Find search field and enter Porsche
-        #enter txt via send _key
-        searchinput = self.driver.find_element_by_id("input-search")    # this finds the search field
-        searchinput.send_keys("Porsche")                                # this enters porsche
+        # find search field and enter Porsche, enter txt via send _key
+        searchinput = self.driver.find_element_by_id("input-search")
+        searchinput.send_keys("Porsche")
         sleep(5)
-        searchinput.send_keys(Keys.RETURN)                              # this sends the return stroke cleanig dropdown
-        sleep(20)
+        searchinput.send_keys(Keys.RETURN)
+        sleep(5)
 
-       #now get the models from the page
+        # clicking drpdwn
+        drpdwn = self.driver.find_element(By.XPATH, "//select[@name='serverSideDataTable_length']")
+        drpdwn.click()
+        sleep(5)
 
+        # drpdwn object is zero based.  100 is 2, so get what is in pos 2 and click it
+        #select = Select(driver.find_element_by_id('serverSideDataTable_length'))
+        #nums = select.select_by_value('100')
+        ok = self.driver.find_elements(By.XPATH("//select[@name='serverSideDataTable_length']/option[value()='100']"))
+        print(ok)
+        sleep(5)
 
+        #options = self.driver.find_element(By.XPATH, "//select[@name='serverSideDataTable_length']/option")
+        #options[2].click()
+        #sleep(5)
+
+        #opt = self.driver.find_element(By.XPATH, "//select[@name='serverSideDataTable_length']")
+        #selector = Select(opt)
+        #opt.click()
+        #sleep(5)
+
+        # now get the models from the page
+        nummod = self.driver.find_elements(By.XPATH, "//*[@id='serverSideDataTable']/tbody/tr/td[6]/span")
+        num = len(nummod)
+
+        # iterate
+        for i in range(num):
+            models = "//*[@id='serverSideDataTable']/tbody/tr[{}]/td[6]/span".format(i + 1)
+            print(models)
 
 
         """
@@ -57,14 +83,7 @@ class Challenge5(unittest.TestCase):
 
      
 
-        #change show entries from 20 to 100
-        drpEntys = self.driver.find_element(By.XPATH, "//select[@name='serverSideDataTable_length']")
-        drpEntys.click()
-        sleep(10)
-
-        options = self.driver.find_element(By.XPATH, "//select[@name='serverSideDataTable_length']/option")
-        options[2].click()
-        sleep(5)
+        
 
         
         # verify Porsche in title
