94c6cd529c86e86421d916b9aa43b26f6c6d098f
cvolden-hub <cvolden@doterra.com>
2019-11-30 17:19:27 -0700

    updates to challenge4 reading and printing from dictionarys-yeah me

--- challenge4/challenge4.py
+++ challenge4/challenge4.py
@@ -26,47 +26,110 @@ class Challenge4(unittest.TestCase):
 
     def test_challange4(self):
 
-        fibval = (fibFuncs.fibLooping(8))  # seting fibval to the value in the nth position (zero based) in fibFuncs
+        fibval = (fibFuncs.fibLooping(25))  # seting fibval to the value in the nth position (zero based) in fibFuncs
+        #fibval =17908
+        print(fibval)
 
-        #nested_dict goes here
-        # oneplace values
-        onesplace = {0: "", 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight',
+        # ones
+        ones = {0: "", 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight',
                      9: 'Nine', 10: 'ten'}
 
-        teens = {11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', 15: 'Fifteen', 16: 'Sixteen',
+        teens = {0: "", 10: 'Ten', 11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', 15: 'Fifteen', 16: 'Sixteen',
                       17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
 
-        #tenslace vaues
-        tensplace = {0: "", 1: 'Ten', 2: 'Twenty', 3: 'Thirty', 4: 'Forty', 5: 'Fifty', 6: 'Sixty', 7: 'Seventy', 8: 'Eighty',
+        #tens
+        tens = {0: "", 1: 'Ten', 2: 'Twenty', 3: 'Thirty', 4: 'Forty', 5: 'Fifty', 6: 'Sixty', 7: 'Seventy', 8: 'Eighty',
                      9: 'Ninety'}
 
-        #hundredsplace values
-        hundredsplace = {0: "", 1: "One Hundred", 2: "Two Hundred", 3: "Three Hundred", 4: "Four Hundred",
+        #hundreds
+        hundreds = {0: "", 1: "One Hundred", 2: "Two Hundred", 3: "Three Hundred", 4: "Four Hundred",
                          5: "Five Hundred", 6: "Six Hundred", 7: "Seven Hundred", 8: "Eight Hundred",
                          9: "Nine Hundred"}
 
-        #thousandplace values
-        thousandplace = {0: "", 1: 'One Thousand', 2000: 'Two Thousand', 3000: 'Three Thousand', 4000: 'Four Thousand',
-                         5000: 'Five Thousand', 6000: 'Six Thousand', 7000: 'Seven Thousand', 8000: 'Eight Thousadn',
-                         9000: 'Nine Thousand'}
+        #thousands
+        thousands = {0: "", 1: 'One Thousand', 2: 'Two Thousand', 3: 'Three Thousand', 4: 'Four Thousand',
+                          5: 'Five Thousand', 6: 'Six Thousand', 7: 'Seven Thousand', 8: 'Eight Thousand',
+                          9: 'Nine Thousand'}
 
-        #tenthousannsplace values
-        tenthousandsplace = {0: "", 10: 'Ten Thousand'}
+        #tenthousands
+        tenthousands = {0: "", 1: 'Ten Thousand', 2: 'Twenty Thousand', 3: 'Thirty Thousand', 4: 'Forty Thousand',
+                          5: 'Fifty Thousand', 6: 'Sixty Thousand', 7: 'Seventy Thousand', 8: 'Eighy Thousand',
+                          9: 'Ninty Thousand'}
 
-        if fibval < 10:
-            intval = onesplace.get(int(fibval))
+        #ones
+        if fibval <=10:
+            intval = ones.get(int(fibval))
             print(fibval, "-", str(intval))
 
+        #teens
         elif fibval <20:
              intval = teens.get(int(fibval))
              print(fibval, " - ", str(intval))
 
-        elif fibval > 19 < 100:
+        #tens
+        elif fibval <100:
              intval = []
              intval += str(fibval)
 
-             what = intval.index(tensplace[2])
+             indexzero = tens.get(int(intval[0]))
+             indexone = ones.get(int(intval[1]))
 
+             print(fibval, "-", indexzero, indexone)
+
+        #humdreds
+        elif fibval <1000:
+             intval = []
+             intval += str(fibval)
+
+             indexzero = hundreds.get(int(intval[0]))
+             indexone = tens.get(int(intval[1]))
+             indextwo = ones.get(int(intval[2]))
+
+             print(fibval, "-", indexzero, indexone, indextwo)
+
+        #thousands
+        elif fibval <10000:
+            intval = []
+            intval += str(fibval)
+
+            indexzero = thousands.get(int(intval[0]))
+            indexone = hundreds.get(int(intval[1]))
+            indextwo = tens.get(int(intval[2]))
+            indexthree = ones.get(int(intval[3]))
+
+            print(fibval, "-", indexzero, indexone, indextwo, indexthree)
+
+        #tenthousands
+        elif fibval < 100000:
+            intval = []
+            intval += str(fibval)
+
+            indexzero = tenthousands.get(int(intval[0]))
+            indexone = thousands.get(int(intval[1]))
+            indextwo = hundreds.get(int(intval[2]))
+            indexthree = tens.get(int(intval[3]))
+            indexfour = ones.get(int(intval[4]))
+
+            firstwo = int(str(fibval)[:2])
+            lastwo = int(str(fibval)[-2:])
+
+            subindex = ones.get(int(lastwo))
+            subindex2 = teens.get(int(lastwo))
+
+            if indexone =='' and lastwo <10:
+                print(fibval, "-", indexzero, indexone, indextwo, subindex)
+            elif indexone =='' and lastwo <20:
+                print(fibval, "-", indexzero, indexone, indextwo, indexthree, indexfour, subindex2)
+            else:
+                index = teens.get(int(firstwo))
+                indexzero = hundreds.get(int(intval[0]))
+                indexone = tens.get(int(intval[1]))
+                indextwo = hundreds.get(int(intval[2]))
+                indexthree = tens.get(int(intval[3]))
+                indexfour = ones.get(int(intval[4]))
+                print(fibval, "-", index, "Thousand", indextwo, indexthree, indexfour, subindex, subindex2)
+
+        """
              for i in range(len(intval -1)):
                  print(tensplace,str(intval))
                  
@@ -99,7 +162,7 @@ class Challenge4(unittest.TestCase):
             intval = tensplace.get[int(fibval[int(i)])]
             print(fibval, "-", str(intval))
 
-        """
+      
         so if the value is > 19 < 100
         split number into parts in a list
         get the len of list
@@ -111,7 +174,6 @@ class Challenge4(unittest.TestCase):
         etc    
     
         """
-
 
 
 if __name__ == '__main__':
